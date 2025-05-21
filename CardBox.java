bfea8e3411a2869119c0bf8b7d405993f7e014b2
SpongySage <164092529+SpongySage@users.noreply.github.com>
2025-05-22 11:38:27 +1200

    + printAllConsole + isCardInBox + get/clearFoundCard + displayCard - working

--- CardBox.java
+++ CardBox.java
@@ -11,7 +11,7 @@ public class CardBox
     // instance variables - replace the example below with your own
     private HashMap<Integer, Card> box; // "Integer" is id, "Card" is the card
     private int cardId; // stores card id to add to new cards
-    
+    private Card foundCard;
     // constants
     static final String DEFAULTIMAGE = "defaultImage.png";
 
@@ -29,8 +29,6 @@ public class CardBox
         addCard("Bulbasaur", 3);
         addCard("Squirtle", 6.98);
         addCard("Charmander", 8.09);
-        
-        printCardBoxToConsole();
     }
     
     /**
@@ -47,17 +45,56 @@ public class CardBox
         genNextId();
         box.put(cardId, new Card(cardId, name, value, image));
     }
-    
     public void addCard(String name, double value){ // String image not passed in
         genNextId();
         box.put(cardId, new Card(cardId, name, value, DEFAULTIMAGE));  // sets image to default
     }
+
+    public void printAllGUI(){
+        for (int cardKey : box.keySet()){
+            UI.println(cardKey + " Details: ");
+            UI.println(box.get(cardKey).getName() + " "
+            + box.get(cardKey).getValue());        
+        }
+    }
     
-    public void printCardBoxToConsole(){
+    public void printAllConsole(){
         for (int cardKey : box.keySet()){
             System.out.println(cardKey + " Details: ");
             System.out.println(box.get(cardKey).getName() + " "
             + box.get(cardKey).getValue());        
         }
     }
+    
+    public Card getFoundCard(){
+        return foundCard;
+    }
+    public void clearFoundCard(){ // clears the found card var
+        foundCard = null;
+    }
+    
+    public boolean isCardInBox(String cardName){
+        for (int cardKey : box.keySet()){
+            if (box.get(cardKey).getName().toLowerCase().trim().equals(cardName)){
+                foundCard = box.get(cardKey);
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    public void displayCard(Card card){
+        UI.println("Card Name: " + card.getName());
+        UI.println("Card Value: $" + card.getValue());
+    
+        int locX = 100; // image x start position
+        int locY = 100; // image y start position
+        
+        final double WIDTH = 250;
+        final double HEIGHT = 300;
+        
+        UI.drawImage(card.getImage(), locX, locY, WIDTH, HEIGHT);
+        
+    }
+    
 }
