8e57712cfeb0999591e4a8c3cf1937017b1508c5
SpongySage <164092529+SpongySage@users.noreply.github.com>
2025-05-21 14:28:26 +1200

    Empty CardBox Class

---
+++ CardBox.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class CardBox here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CardBox
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class CardBox
+     */
+    public CardBox()
+    {
+        // initialise instance variables
+        
+    }
+}
