c03d4db679394d43716785e0df15defd39078b8b
SpongySage <164092529+SpongySage@users.noreply.github.com>
2025-05-21 14:28:02 +1200

    Empty Card Class

---
+++ Card.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Cards here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Card
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class Cards
+     */
+    public Card()
+    {
+        // initialise instance variables
+        
+    }
+}
