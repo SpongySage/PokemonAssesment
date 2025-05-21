575ba9f89c2a809fa701b0b012b40568577ba34a
SpongySage <164092529+SpongySage@users.noreply.github.com>
2025-05-21 15:20:09 +1200

    + Attributes + Getters + Constructor | Card is functioning

--- Card.java
+++ Card.java
@@ -8,13 +8,49 @@
 public class Card
 {
     // instance variables - replace the example below with your own
-
+    private int id;
+    private String name;
+    private double value;
+    private String image;
+    
+    // constants
+    static final String DEFAULTIMAGE = "defaultImage.png";
+        
     /**
      * Constructor for objects of class Cards
+     * Used when String img IS passed in
      */
-    public Card()
+    public Card(int key, String nm, double val, String img)
     {
         // initialise instance variables
-        
+        id = key;
+        name = nm;
+        value = val;
+        image = img;
+    }
+    
+    /**
+     * Gets id
+     */
+    public int getId(){
+        return id;
+    }
+    /**
+     * Gets name
+     */
+    public String getName(){
+        return name;
+    }    
+    /**
+     * Gets value
+     */
+    public double getValue(){
+        return value;
+    }
+    /**
+     * Gets image
+     */
+    public String getImage(){
+        return image;
     }
 }
