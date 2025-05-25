2a168e5a68c738453d45010f4cd28966cd820c8c
SpongySage <164092529+SpongySage@users.noreply.github.com>
2025-05-26 11:01:48 +1200

    + doMouse() - clear display + mouse testing method + cap on hash leng + commenting

--- GUI.java
+++ GUI.java
@@ -4,12 +4,16 @@ import java.util.HashMap;
  * GUI class
  * GUI has buttons and a search bar
  * a graphics and text pane
+ * GUI does: search for a card using textField, clear UI panes, add card, take mouse input
+ * 
+ * @author Sage Ripley-Cornwell
  */
 public class GUI
 {
-    // instance variables - replace the example below with your own
+    // instance variables
     private CardBox cardBox; // creates instance of CardBox
-    private Card card;
+    private Card card; // creates instance of Card
+    private boolean cardDisplayed; // t/f is card image displayed
     
     /**
      * Constructor for objects of class GUI
@@ -18,7 +22,9 @@ public class GUI
     {
         // initialise instance variables
         cardBox = new CardBox();
+        cardDisplayed = false;
         
+        // initialise GUI
         UI.initialise();
         
         // Text Field, search bar
@@ -32,6 +38,9 @@ public class GUI
         
         // mouse listener
         UI.setMouseListener(this::doMouse);
+        
+        // testing button, change to different methods as needed
+        UI.addButton("Test", this::printAllVar);
     }
     
     /**
@@ -45,10 +54,11 @@ public class GUI
             if (cardBox.isCardInBox(cardSearch.toLowerCase().trim())){
                 UI.println("Card found!");
                 
-                card = cardBox.getFoundCard();
-                cardBox.clearFoundCard(); // clears found card, not used again
+                card = cardBox.getFoundCard(); // gets card info of card that was found
+                cardBox.clearFoundCard(); // clears found card, so not used again
                 
                 cardBox.displayCard(card); // displays card info and image
+                cardDisplayed = true;
                 UI.println();
             }else{
                 UI.println("Card could not be found");
@@ -56,35 +66,100 @@ public class GUI
         }
     }
     
+    /**
+     * clears text and graphics pane
+     * sets cardDisplayed to false as panes are now clear
+     */
     public void clearPanes(){
         UI.clearText();
         UI.clearGraphics();
+        cardDisplayed = false;
     }
     
+    /**
+     * Adds card
+     * prompts user to enter card details - name, value, image
+     * if hashmap leng is larger than max, doesnt add card
+     * user must enter valid value between 0 and max
+     */
     public void addCard(){
         final int MAX_LENGTH = 50; // max amount of obj in hashmap
         final int MAX_VALUE = 5275000; // max pokemon card value based on highest value card
         double value;
         
-        // ask user for card info
-        String name = UI.askString("Pokémon Name: ");
-        do {
-            value = UI.askDouble("Value Of Card: $");
-            if (value <= 0){
-                UI.println("Card value must be higher than 0");
-            }else if (value > MAX_VALUE){
-                UI.println("Card value must be below $" + MAX_VALUE);
-            }
-        }while (value < 0 || value > MAX_VALUE);
-        String imageFile = UIFileChooser.open("Select Image: ");
+        if (cardBox.getBoxLeng() <= MAX_LENGTH){
+            // ask user for card info
+            String name = UI.askString("Pokémon Name: ");
+            
+            // stops user from entering value more than max or less than 0
+            do {
+                value = UI.askDouble("Value Of Card: $");
+                if (value <= 0){
+                    UI.println("Card value must be higher than 0");
+                }else if (value > MAX_VALUE){
+                    UI.println("Card value must be below $" + MAX_VALUE);
+                }
+            }while (value < 0 || value > MAX_VALUE);
+            // prompts user to select image
+            String imageFile = UIFileChooser.open("Select Image: ");
+            
+            // adds card to hashmap
+            cardBox.addCard(name, value, imageFile);
+            UI.println();
+            UI.println(name + " has been added");
+            UI.println();
+        }else{
+            UI.println("Sorry, your collection is full.");
+            UI.println("No more cards can be added.");
+            UI.println();
+        }
+    }
         
-        cardBox.addCard(name, value, imageFile);
-        UI.println();
-        UI.println(name + " has been added");
-        UI.println();
+    /**
+     * Mouse input
+     * if the mouse is clicked and the card image is displayed,
+     * checks mouse loc is in image loc, if it is, clears panes
+     */
+    public void doMouse(String action, double x, double y){
+        if (action.equals("clicked") && cardDisplayed){
+            if ((x > cardBox.getImgLeft()) && (x < cardBox.getImgRight()) && (y > cardBox.getImgTop()) && (y < cardBox.getImgBottom())){
+                clearPanes();
+            }
+        }
     }
     
-    public void doMouse(String action, double xPos, double yPos){
+    
+    /**
+     * Testing methods
+     */
+    /**
+     * tests mouse input handling
+     * prints results to console
+     */
+    public void doMouseTest(String action, double x, double y){
+        if (action.equals("clicked")){
+            System.out.println("clicked");
+            if (cardDisplayed){
+                System.out.println("Clicked + card visable");
+                if ((x > cardBox.getImgLeft()) && (x < cardBox.getImgRight()) && (y > cardBox.getImgTop()) && (y < cardBox.getImgBottom())){
+                    System.out.println("Image Clicked");
+                }else{
+                    System.out.println("missed!");
+                }
+            }
+        }
+    }
+    /**
+     * prints all values of current variables
+     */
+    public void printAllVar(){
+        System.out.println();
+        System.out.println("______________________________");
+        System.out.println("GUI Class Var:");
+        System.out.println("cardDisplayed: " + cardDisplayed);
+        System.out.println();
         
+        System.out.println("CardBox Class Var:");
+        cardBox.printAllVar();
     }
 }
